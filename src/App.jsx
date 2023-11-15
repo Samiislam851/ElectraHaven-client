b14f8693c96a2d979abe3da634c809c090656a2e
siam <samisiam851@gmail.com>
2023-11-16 01:50:56 +0600

    https problem on live

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,7 @@ import RouteHandle from './Config/RouteHandle';
 import AuthContextProvider from './Provider/AuthContextProvider';
  import axios from 'axios'; 
  axios.defaults.baseURL = "http://backend.sunpacs.com/";
-//  import {
-//   QueryClient,
-//   QueryClientProvider,
-//   useQuery,
-// } from '@tanstack/react-query'
 
-// const queryClient = new QueryClient()
-//  axios.defaults.baseURL = "http://192.168.0.123:5000/"
-//  axios.defaults.baseURL = "https://mongo.raihanmiraj.com/"
  function App() {
  
 
